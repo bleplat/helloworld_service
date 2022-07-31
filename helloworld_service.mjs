a4a0c8530b6be6062ce96463c9451d7ccb1b5bea
redacted <redacted>
2022-07-31 18:02:12 +0200

    printing something

--- helloworld_service.mjs
+++ helloworld_service.mjs
@@ -6,6 +6,7 @@ const port = 7000;
 const app = express();
 
 app.get('/message/**', (req, res) => {
+	console.log("INFO: requested req.params[0]")
 	res.send({ success: true, message: req.params[0] });
 });
 
