ea985ee11871eeb5177ce4c3a4d1723fdbdcb86a
redacted <redacted>
2022-07-31 17:29:54 +0200

    added the service executable

---
+++ helloworld_service.mjs
@@ -0,0 +1,12 @@
+#!/bin/env node
+import express from 'express';
+import path from 'path';
+
+const port = 7000;
+const app = express();
+
+app.get('/message/**', (req, res) => {
+	res.send({ success: true, message: req.params[0] });
+});
+
+app.listen(port);
